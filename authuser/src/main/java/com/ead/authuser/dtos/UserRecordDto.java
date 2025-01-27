e3a9499f58c61734916e015cde65628aea1940f8
Wellington Lima <wellington.map@hotmail.com>
2025-01-27 09:10:48 -0300

    feat(user-service): implement password custom validation

--- authuser/src/main/java/com/ead/authuser/dtos/UserRecordDto.java
+++ authuser/src/main/java/com/ead/authuser/dtos/UserRecordDto.java
@@ -1,5 +1,6 @@
 package com.ead.authuser.dtos;
 
+import com.ead.authuser.validations.PasswordConstraint;
 import com.fasterxml.jackson.annotation.JsonView;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -18,11 +19,13 @@ public record UserRecordDto(
 
                             @NotBlank(groups = {UserView.RegistrationPost.class, UserView.PasswordPut.class}, message = "Password is mandatory")
                             @Size(min = 6, max = 20, groups = {UserView.RegistrationPost.class, UserView.PasswordPut.class}, message = "Size must be between 6 and 20")
+                            @PasswordConstraint(groups = {UserView.RegistrationPost.class, UserView.PasswordPut.class})
                             @JsonView({UserView.RegistrationPost.class, UserView.PasswordPut.class})
                             String password,
 
                             @NotBlank(groups = UserView.PasswordPut.class, message = "Old Password is mandatory")
                             @Size(min = 6, max = 20, groups = UserView.PasswordPut.class, message = "Size must be between 6 and 20")
+                            @PasswordConstraint(groups = UserView.PasswordPut.class)
                             @JsonView(UserView.PasswordPut.class)
                             String oldPassword,
 

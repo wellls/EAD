82d8c36a4b541135277578c7292508247ff57ebc
Wellington Lima <wellington.map@hotmail.com>
2025-02-01 09:27:16 -0300

    feat(course-service): delete a module and all lessons into this module

--- course/src/main/java/com/ead/course/services/impl/ModuleServiceImpl.java
+++ course/src/main/java/com/ead/course/services/impl/ModuleServiceImpl.java
@@ -1,13 +1,31 @@
 package com.ead.course.services.impl;
 
+import com.ead.course.models.LessonModel;
+import com.ead.course.models.ModuleModel;
+import com.ead.course.repositories.LessonRepository;
 import com.ead.course.repositories.ModuleRepository;
 import com.ead.course.services.ModuleService;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 public class ModuleServiceImpl implements ModuleService {
 
     final ModuleRepository moduleRepository;
+    final LessonRepository lessonRepository;
 
-    public ModuleServiceImpl(ModuleRepository moduleRepository) {
+    public ModuleServiceImpl(ModuleRepository moduleRepository, LessonRepository lessonRepository) {
         this.moduleRepository = moduleRepository;
+        this.lessonRepository = lessonRepository;
+    }
+
+    @Transactional
+    @Override
+    public void delete(ModuleModel moduleModel) {
+        List<LessonModel> lessonModelList = lessonRepository.findAllLessonsIntoModule(moduleModel.getModuleId());
+        if(!lessonModelList.isEmpty()) {
+            lessonRepository.deleteAll(lessonModelList);
+        }
+        moduleRepository.delete(moduleModel);
     }
 }

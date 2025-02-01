c89ad397131b23f45c2222161d7ff24d304d406f
Wellington Lima <wellington.map@hotmail.com>
2025-02-01 09:22:14 -0300

    feat(course-service): delete a course with all modules and lessons

--- course/src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
+++ course/src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
@@ -1,15 +1,48 @@
 package com.ead.course.services.impl;
 
+import com.ead.course.models.CourseModel;
+import com.ead.course.models.LessonModel;
+import com.ead.course.models.ModuleModel;
 import com.ead.course.repositories.CourseRepository;
+import com.ead.course.repositories.LessonRepository;
+import com.ead.course.repositories.ModuleRepository;
 import com.ead.course.services.CourseService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class CourseServiceImpl implements CourseService {
 
     final CourseRepository courseRepository;
+    final ModuleRepository moduleRepository;
+    final LessonRepository lessonRepository;
 
-    public CourseServiceImpl(CourseRepository courseRepository) {
+    public CourseServiceImpl(CourseRepository courseRepository, ModuleRepository moduleRepository, LessonRepository lessonRepository) {
         this.courseRepository = courseRepository;
+        this.moduleRepository = moduleRepository;
+        this.lessonRepository = lessonRepository;
+    }
+
+    @Transactional
+    @Override
+    public void delete(CourseModel courseModel) {
+        //search modules
+        List<ModuleModel> moduleModelList = moduleRepository.findAllModulesIntoCourse(courseModel.getCourseId());
+        if(!moduleModelList.isEmpty()) {
+            for(ModuleModel module : moduleModelList) {
+                //search lessons for each model
+                List<LessonModel> lessonModelList = lessonRepository.findAllLessonsIntoModule(module.getModuleId());
+                if(!lessonModelList.isEmpty()) {
+                    //delete all lessons into a module
+                    lessonRepository.deleteAll(lessonModelList);
+                }
+            }
+
+            //delete all modules into a course
+            moduleRepository.deleteAll(moduleModelList);
+        }
+        courseRepository.delete(courseModel);
     }
 }
